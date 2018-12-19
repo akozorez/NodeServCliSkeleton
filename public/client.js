826b3dad6bb0ab34dd8458a31d46379673b150e6
Alexander <kozzorez@yandex.ru>
2018-12-20 00:58:34 +0200

    added auth, sessions, styles changed

--- public/client.js
+++ public/client.js
@@ -1,10 +1,13 @@
 ;(function() {
-
-    // this.get - функция, которая делает GET с клиента
+    var body = document.querySelector('body');
+    var current_page = body.getAttribute('data-page-name');
+    var app = document.querySelector('#app');
     var XMLHttpReq = function() {
-        this.get = function(uri, callback) {
+        // this.get - метод, который делает GET с клиента,
+        // Объект обязательно создавать через new!
+        this.get = (uri, callback) => {
             var XML = new XMLHttpRequest();
-            XML.onreadystatechange = function() {
+            XML.onreadystatechange = () => {
                 if (XML.readyState == 4 && XML.status == 200)
                     callback(XML.responseText)
             }
@@ -13,25 +16,26 @@
         }
     }
     var client = new XMLHttpReq();
-    /*
-        Делайте GET запросы с клиента на сервер, с помощью:
-        client.get('/url', function(response){
-            console.log(response);
-        });
-    */ 
-    client.get('/hello-world', function(response){
-        // Выборка селектора с id = app
-        var app = document.querySelector('#app');
-        // Создание параграфа с соответствующим текстом внутри
-        var p = document.createElement('p');
-        p.innerText = response;
-        // Добавление параграфа внутри #app
-        app.appendChild(p);
-        // Создание ссылки на /my-collection и добавление внутри #app
-        var a = document.createElement('a');
-        a.setAttribute('href','/my-collection');
-        a.innerText = 'Посмотреть базу данных';
-        app.appendChild(a);
-    });
 
+    if (current_page == 'homepage') {
+        /*
+            Делайте GET запросы с клиента на сервер, с помощью:
+            client.get('/url', function(response){
+                console.log(response);
+            });
+        */ 
+        client.get('/hello', (response) => {
+            // Создание параграфа с response.message, полученное из базы данных внутри
+            var p = document.createElement('p');
+            p.innerText = response;
+            // Добавление параграфа внутри #app
+            app.append(p)
+        })
+    }
+    if (current_page == 'register') {
+        console.log('register')
+    }
+    if (current_page == 'login') {
+        console.log('login')
+    }
 })();
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-
-// Подключение пакетов
+// Подключение пакетов/зависимостей
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
@@ -7,54 +6,196 @@ var fs = require('fs');
 var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-
+var expressSession = require('express-session');
 // Инициализация конфигурации
 var assets = path.join(__dirname, '.', 'public');
 var charset = 'utf8';
 var app = express();
-var port = process.env.PORT || 3000;
+var server_port = process.env.PORT || 3000;
+var server_name = 'localhost';
+var db_port = 27017;
+var db_url = 'mongodb://' + server_name + ':' + db_port + '/';
 var db_;
-
-// Подключение конфигураций
+// Подключение конфигурации
 app.use(bodyParser.urlencoded({
-        extended: !1
-    }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/public/views');
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
-
+    extended: !1
+}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+app.use(expressSession({
+    secret: 'privetiki',
+    saveUninitialized: !1,
+    resave: !1,
+    maxAge: new Date(Date.now() + 3600000),
+    expires: new Date(Date.now() + 3600000)
+}))
+app.set('views', __dirname + '/public/views')
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 // GET & POST запросы
-app.get('/', function(req, res) {
-    res.status(200).render('index.html')
-});
-app.get('/hello-world', function(req, res) {
+app.get('/', (req, res, next) => {
+    console.log('[GET] /')
+    var err = req.session.errors;
+    if (err) req.session.errors = null;
+    res.render('index', {
+        success: req.session.success,
+        errors: err,
+        logined: req.session.logined
+    })
+})
+app.get('/hello', (req, res, next) => {
+    console.log('[GET] /hello')
     // Поиск в коллекции объекта с data
-    var data = {type:'string'};
-    db_.collection('my-collection').findOne(data, function(err, response){
-        if(err){
+    var data = {
+        type: 'string'
+    };
+    db_.collection('hello').findOne(data, (err, response) => {
+        if (err) {
             console.log(err);
-            return res.sendStatus(500);
+            return res.status(500)
         }
-        res.status(200).send(response.message);
-    });
-});
-app.get('/my-collection', function(req, res){
-    // Возвращение коллекции как JSON
-    db_.collection('my-collection').find().toArray(function(err, data){
-        if(err) {
-            console.log(err);
-            return res.sendStatus(500);
+        res.status(200).send(response.message)
+    })
+})
+app.get('/register', (req, res, next) => {
+    console.log('[GET] /register')
+    if (!req.session.logined) {
+        var err = req.session.errors;
+        if (err) req.session.errors = null;
+        res.render('register', {
+            success: req.session.success,
+            errors: err,
+            logined: req.session.logined
+        })
+    } else res.redirect('/')
+})
+app.post('/register', (req, res, next) => {
+    console.log('[POST] /register');
+    // Ошибки
+    const passwords_not_match = 'Пароли не совпадают';
+    const passwords_is_less = 'Пароль должен содержать от 6 символов';
+    const login_is_less = 'Логин должен содержать от 6 символов';
+    const login_is_busy = 'Такой логин уже существует';
+    // Без ошибок
+    const success_register = 'Аккаунт успешно создан';
+    // Регистрация
+    ((login, psw, repeat_psw) => {
+        if (psw != repeat_psw) {
+            req.session.errors = passwords_not_match;
+            req.session.success = !1;
+            res.redirect('/register')
+        } else if (psw.length < 6) {
+            req.session.errors = passwords_is_less;
+            req.session.success = !1;
+            res.redirect('/register')
+        } else if (login.length < 6) {
+            req.session.errors = login_is_less;
+            req.session.success = !1;
+            res.redirect('/register')
+        } else {
+            var search = {
+                login: login
+            };
+            db_.collection('users').findOne(search, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500)
+                }
+                if (data !== null) {
+                    req.session.errors = login_is_busy;
+                    req.session.success = !1;
+                    res.redirect('/register')
+                } else {
+                    var data = {
+                        login: login,
+                        password: psw
+                    };
+                    db_.collection('users').insertOne(data, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500)
+                        }
+                        console.log('Аккаунт ' + login + ' создан');
+                        req.session.errors = success_register;
+                        req.session.success = !0;
+                        res.redirect('/login')
+                    })
+                }
+            })
         }
-        res.send(data);
-    });
-});
-
-// Подключение к базе данных
-MongoClient.connect('mongodb://localhost:27017/', {
-    useNewUrlParser: !0
-    }, function(err, database) {
+    })(req.body.login, req.body.psw, req.body.psw_repeat)
+})
+app.get('/login', (req, res, next) => {
+    console.log('[GET] /login')
+    if (!req.session.logined) {
+        var err = req.session.errors;
+        if (err) req.session.errors = null;
+        res.render('login', {
+            success: req.session.success,
+            errors: err,
+            logined: req.session.logined
+        })
+    } else res.redirect('/')
+})
+app.post('/login', (req, res, next) => {
+    console.log('[POST] /login')
+    // Ошибки
+    const passwords_incorrect = 'Данные не удовлетворяют нашим записям';
+    const login_is_incorrect = 'Такого логина не существует';
+    // Без ошибок
+    const success_login = 'Вы успешно вошли в систему';
+    // Авторизация
+    ((login, psw) => {
+        var search = {
+            login: login
+        };
+        db_.collection('users').findOne(search, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(500)
+            }
+            var data = JSON.parse(JSON.stringify(data));
+            if (data !== null) {
+                if (data.password != psw) {
+                    req.session.errors = passwords_incorrect;
+                    req.session.success = !1;
+                    res.redirect('/login')
+                } else {
+                    req.session.errors = success_login;
+                    req.session.success = !0;
+                    req.session.logined = !0;
+                    res.redirect('/')
+                }
+            } else {
+                req.session.errors = login_is_incorrect;
+                req.session.success = !1;
+                res.redirect('/login')
+            }
+        })
+    })(req.body.login, req.body.psw)
+})
+app.get('/logout', (req, res, next) => {
+    console.log('[GET] /logout')
+    if (!req.session.logined) {
+        res.redirect('/')
+    } else {
+        req.session.errors = 'Вы успешно вышли из системы';
+        req.session.success = !0;
+        req.session.logined = !1;
+        res.redirect('/')
+    }
+})
+// Редирект на 404, после - не должно быть GET-ов
+app.get('*', (req, res, next) => {
+    console.log('[GET] /*?404')
+    res.status(404).render('404.html')
+})
+// Создание сервера на заданном порте
+app.listen(server_port, () => {
+    // Подключение к базе данных
+    MongoClient.connect(db_url, {
+        useNewUrlParser: !0
+    }, (err, database) => {
         if (err) {
             return console.log(err)
         }
@@ -63,29 +204,64 @@ MongoClient.connect('mongodb://localhost:27017/', {
         * Здесь можете сделать импорт базы данных
         * который будет сделан единожды, при первом запуске сервера
         */
-        db_.collection('my-collection').countDocuments(function(err, count){
-            if(!err && count === 0){
-
-
-                // Вставка в коллекцию объекта data (Коллекция автоматически создается при объявлении)
-                var data = {type:'string', message:'Приветики пистолетики!'};
-                db_.collection('my-collection').insertOne(data, function(err, result){
-                    if(err){
+        db_.collection('hello').countDocuments((err, count) => {
+            if (!err && count === 0) {
+                // Вставка в коллекцию объекта data 
+                // (Коллекция автоматически создается при объявлении)
+                var data = {
+                    type: 'string',
+                    message: 'приветики пистолетики!'
+                };
+                db_.collection('hello').insertOne(data, (err, result) => {
+                    if (err) {
                         console.log(err);
-                        return res.sendStatus(500);
+                        return res.status(500)
                     }
-                    console.log('Коллекция my-collection создана');
-                });
+                    console.log('Коллекция hello создана')
+                })
             }
-        });
-});
-
-// Редирект на 404, после - не должно быть GET-ов
-app.get('*',function(req, res){
-    res.status(404).render('404.html');
-});
-
-// Создание сервера на заданном порте
-app.listen(port, function() {
-    console.log('Это работает: http://localhost:'+port);
+        })
+        // Инициализация и валидация полей пользователя 
+        // (Коллекция автоматически создается при объявлении)
+        // Этот код лучше не трогать
+        db_.collection('users').countDocuments((err, count) => {
+            if (!err && count === 0) {
+                // Функция создания коллекции пользователей
+                (() => {
+                    db_.collection('users', {
+                        validator: {
+                            $jsonSchema: {
+                                bsonType: "object",
+                                required: ["login", "password"],
+                                properties: {
+                                    login: {
+                                        bsonType: "string",
+                                        description: "must be a string and is required"
+                                    },
+                                    password: {
+                                        bsonType: "string",
+                                        description: "must be a string and is required"
+                                    }
+                                }
+                            }
+                        },
+                        validationAction: "warn"
+                    })
+                    var data = {
+                        login: 'admin',
+                        password: 'admin'
+                    };
+                    db_.collection('users').insertOne(data, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500)
+                        }
+                        console.log('Аккаунт admin создан')
+                    })
+                    console.log('Коллекция users создана')
+                })()
+            }
+        })
+    })
+    console.log('Это работает: http://localhost:' + server_port)
 })
